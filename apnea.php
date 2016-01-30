a7d47074b88bab19cefcc67759a8f2042dfd26e0
da <da@deepthought>
2016-01-30 21:43:35 +0100

    no idea if i changed anything or just adjusted timestamps :]

--- apnea.php
+++ apnea.php
@@ -4,8 +4,8 @@ class Entry {
 	private $rdi;
 	private $leakage;
 	private $pressure;
-	private $time_slept;
-	private $date;
+	public $time_slept;
+	public $date;
 
 	function __construct ($d, $m, $y, $rdi, $leakage, $pressure, $time_slept) {
 		$this->date = new DateTime();
@@ -13,8 +13,8 @@ class Entry {
 		$this->date->setTime(0, 0, 0);
 		$this->rdi = trim(str_replace(',', '.', $rdi));
 		$this->leakage = trim(str_replace(',', '.', $leakage));
-		$this->pressure = $pressure;
-		$this->time_slept = $time_slept;
+		$this->pressure = trim(str_replace(',', '.', $pressure));
+		$this->time_slept = trim($time_slept);
 	}
 
 	public function is_valid() {
@@ -38,7 +38,7 @@ class Entry {
 	
 }
 
-$csv = explode("\n", file_get_contents('WM_DIARY.csv'));
+$csv = explode("\n", file_get_contents('WM_DIARY.CSV'));
 $csv = array_slice($csv, 7); // skip entries without data
 
 /*
@@ -65,19 +65,42 @@ $csv = array_slice($csv, 7); // skip entries without data
  */
 $entries = array();
 foreach ($csv as $entry) {
+	//echo $entry . PHP_EOL;
 	$entry = explode(';', $entry);
+	if (sizeof($entry) === 1) continue; 
 	$e = new Entry($entry[1], $entry[2], $entry[3], $entry[6], $entry[10], $entry[9], $entry[4]);	
 	if (!$e->is_valid()) continue;
 	$entries []= $e;
-	echo $e . PHP_EOL;
+	//echo $e . PHP_EOL;
 }
 
 usort($entries, function($a, $b) {
 	return $a->getTimestamp() > $b->getTimestamp();
 });
 
+$m_total = 0;
+$m_cnt = 0;
+$m_cur = 0;
 foreach ($entries as $e) {
-	//echo 'rrdtool update apnea.rrd ' . $e . PHP_EOL;
+	$m = $e->date->format('m');
+	if ($m_cur != $m) {
+		if ($m_cnt > 1) {
+			echo $m_cur . ': ' . round($m_total/$m_cnt) . PHP_EOL;
+		}
+		$m_cur = $m;
+		$m_total = 0;
+		$m_cnt = 0;
+	}
+	if ($e->time_slept < 300 || $e->time_slept > 600) continue;
+	$m_cnt++;
+	$m_total += $e->time_slept;
+}
+			echo $m_cur . ': ' . round($m_total/$m_cnt) . PHP_EOL;
+
+exit;
+
+foreach ($entries as $e) {
+	echo 'rrdtool update apnea.rrd ' . $e . PHP_EOL;
 	shell_exec('rrdtool update apnea.rrd ' . $e);
 }
 echo sizeof($entries);
